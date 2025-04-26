74832ed05110594e8ffcf09514227e9b0986c1af
P Sruthi <sruthipalle6406@gmail.com>
2025-04-27 02:48:44 +0530

    final changes before publish

--- frontend/src/components/EditStudent.js
+++ frontend/src/components/EditStudent.js
@@ -19,7 +19,7 @@ const EditStudent = ({ setStudents }) => {
   useEffect(() => {
     const fetchStudent = async () => {
       try {
-        const res = await fetch(`http://localhost:5000/students/${id}`);
+        const res = await fetch(`https://student-management-system-backend-et8x.onrender.com/students/${id}`);
         const data = await res.json();
         setStudent(data);
       } catch (err) {
